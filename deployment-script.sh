6a2f7e3d34a51e6356c839a2c1a5eb2471427d37
BodgeMaster <>
2023-10-03 19:03:44 +0200

    replace index.php with index.html
    The index doesn’t have to be a PHP file since there is no need to
    generate the list of chapters anew every time the ToC is opened.
    
    Instead, the list of chapters is generated once, ahead of time, and
    saved to the index.html.
    
    As an added bonus, this allows the ToC to be generated and used
    as a local file.

---
+++ deployment-script.sh
@@ -0,0 +1,15 @@
+cp html_resources/index_template.html index.html
+
+function genlinks {
+  for FILE in $(find . -maxdepth 1 -type f -name "*.html"); do
+    echo -ne "  <li><a href=\"$FILE\">\n"
+    grep -oe "<title>.*</title>" $FILE | sed -e 's/<title> /    /;s| </title>||'
+    echo "  </a></li>"
+  done
+}
+
+sed -ne '/  <!-- chapter_list_here -->/q;p' html_resources/index_template.html > index.html
+# for some fucking reason, there is no way to tell wc to only print the number so we do cat abuse to get rid of the filename
+LINE_NUMBER=$(("$(cat index.html | wc -l)"+2))
+genlinks >> index.html
+tail -n+$LINE_NUMBER html_resources/index_template.html >> index.html
