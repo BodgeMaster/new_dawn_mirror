019846a87a743d7f78119125a93652cbdeb33cfa
BodgeMaster <>
2023-10-08 23:05:00 +0200

    deployment-script: Only include chapters in the ToC

--- deployment-script.sh
+++ deployment-script.sh
@@ -2,7 +2,7 @@
 cp html_resources/index_template.html index.html
 
 function genlinks {
-  for FILE in $(find . -maxdepth 1 -type f -name "*.html"); do
+  for FILE in $(find . -maxdepth 1 -type f -name "*-*.html"); do
     echo -ne "  <li><a href=\"$FILE\">\n"
     grep -oe "<title>.*</title>" $FILE | sed -e 's/<title> /    /;s| </title>||'
     echo "  </a></li>"
