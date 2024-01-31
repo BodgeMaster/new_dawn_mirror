5c881b9710064c26a40592bec61cf7d042d6d99e
BodgeMaster <>
2024-01-31 20:38:40 +0100

    deployment-script: log files to stderr as they are added

--- deployment-script.sh
+++ deployment-script.sh
@@ -3,6 +3,7 @@ cp html_resources/index_template.html index.html
 
 function genlinks {
   for FILE in $(find . -maxdepth 1 -type f -name "??-*.html" | sort); do
+    echo "$FILE" >&2
     echo -ne "  <li><a href=\"$FILE\">\n"
     echo -n "    $(sed -e 's|^\./||;s/-.*//' <<< "$FILE") <span class="'"chapter">'
     grep -oe "<title>.*</title>" $FILE | sed -e 's/<title> //;s| </title>|</span>|'
